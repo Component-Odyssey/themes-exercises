a59725cf8ab1d7aff186922ac0e8a132b1a3fff3
Andrico <karoulla.andrico@gmail.com>
2024-02-09 08:49:00 +0000

    add some hints

--- starter/theme-toggler.js
+++ starter/theme-toggler.js
@@ -1,6 +1,11 @@
 import { LitElement, html } from "lit";
 import { styles } from "./src/toggler-styles.js";
 
+// Hint: You'll want to store the list of toggles and track the currently active theme.
+// When you've hit the last theme, you'll want to loop back to the first one.
+// Assume that no theme is currently present on the body element. Clicking the theme toggler for the first time should set the theme to 'red'
+// To get the tests passing, ensure that your theme cycles through the correct order defined in the README.
+
 class ThemeToggler extends LitElement {
   static styles = styles;
 
